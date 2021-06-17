d9af7247a07e8a213aba2d998ca5fd963b63d760
DrelaDominika <dominika.drela@gmail.com>
2021-06-17 15:42:41 +0200

    quote API added

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,24 +1,52 @@
 <template>
-    <div class="overview">
-        Overview
+    <div>
+        <h1 class="overview">Bookland - let the book talk...</h1>
+        <div class="library">
+            <img width="1200" height="675" src="../assets/library.jpg">
+        </div>
+        <div>
+        <vue-inspirational-quote>
+            <template v-slot:default="slotProps">
+            <blockquote class="blockquote quote">
+                &#8220;{{ slotProps.quote.body }}&#8221;
+                <p>
+                    <em>&mdash;{{ slotProps.quote.author }}</em>
+                </p>
+            </blockquote>
+            </template>
+        </vue-inspirational-quote> 
+    </div>
     </div>
 </template>
 
 <script>
+import VueInspirationalQuote from "vue-inspirational-quote"
     export default {
-        
+        components: {
+    VueInspirationalQuote
+    },
     }
 </script>
 
 <style scoped>
 .overview {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
+    margin-top: 30px;
+    font-style: italic;  
     font-size: 40px;
-    color: rgb(167, 167, 167);
-    font-weight: 600;
+    color: rgb(119, 56, 56);
+    font-weight: 550;
 }
+
+.library{
+    margin-top: 30px;
+}
+
+.quote
+    {
+    margin-top: 30px;
+    background-color:rgba(121, 72, 72, 0.116);
+    padding:6px;
+    border-radius: 25px;
+    }
+
 </style>
